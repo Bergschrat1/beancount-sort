ec0da849f2d5fbff8eab3d03185d25b7f6e5b582
Bergschrat1 <jokeh@gmx.de>
2021-09-14 18:01:28 +0200

    fixed repeated section headings

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,7 @@ struct Cli {
 }
 
 const SECTIONS: [&str; 7] = ["Header",  "Accounts", "Options", "Commodities", "Other Entries", "Prices", "Transactions"];
+const NDECO: usize = 4; // number of "$" to use at section headings
 
 #[derive(Debug)]
 struct LedgerFile {
@@ -83,6 +84,7 @@ enum Line {
     Comment,
     Indent,
     Empty,
+    Section,
 }
 
 fn read_file(path: &Path) -> Result<LedgerFile, anyhow::Error> {
@@ -123,10 +125,14 @@ fn get_line_type(line: &str, n: &usize) -> Result<Line, anyhow::Error> {
     let re_comment = Regex::new(r"^(;+)").unwrap();
     let re_indented = Regex::new(r"(?m)(^ +)\S").unwrap();
     let re_empty = Regex::new(r"^.{0}$").unwrap();
+    let re_section = Regex::new(format!("^;{}", "$".repeat(NDECO)).as_str()).unwrap();
     if re_date.is_match(line) {
         Ok(Line::Date(NaiveDate::parse_from_str(&first_thing, "%Y-%m-%d").unwrap()))
     } else if re_option.is_match(line) {
         Ok(Line::Option)
+    // section has to be tested before comment
+    } else if re_section.is_match(line) {
+        Ok(Line::Section)
     } else if re_comment.is_match(line) {
         Ok(Line::Comment)
     } else if re_indented.is_match(line) {
@@ -175,17 +181,19 @@ fn find_entries(mut ledger_file: LedgerFile, n_skip: usize) -> Result<LedgerFile
             // Check start of each line
             //  - date
             //      - Check type
-            //      - If multiline, keep reading
             //  - option
             //      - save
+            //  - section
+            //      - ignore
             //  - comment
-            //      - Ignore
+            //      - save and prepend to next line
             //  - indented
             //      - add to previous transaction
             //  - empty
             //      - Ignore
             Line::Date(d) => construct_dated_entry(&line, d).unwrap(),
             Line::Option => Entry{content: line.to_owned(), date: NaiveDate::from_ymd(1990, 1, 1), entry_type: EntryType::Option},
+            Line::Section => continue,
             Line::Comment => Entry{content: line.to_owned(), date: NaiveDate::from_ymd(1990, 1, 1), entry_type: EntryType::Comment},
             Line::Indent => Entry{content: line.to_owned(), date: NaiveDate::from_ymd(1990, 1, 1), entry_type: EntryType::Indented},
             Line::Empty => continue,
@@ -237,12 +245,12 @@ fn get_section_variant(entry: &str) -> Result<EntryType, anyhow::Error> {
 
 fn sort_entries(entries: Vec<Entry>) -> Result<Vec<Entry>, anyhow::Error> {
     let mut sorted_entries: Vec<Entry> = Vec::new();
-    let deco = ";;;;".to_string();
+    let deco = "€".repeat(NDECO);
     for section in SECTIONS {
         if section != "Header" {
-            let section_string = {deco.clone() + &";".repeat(section.len()) + &deco + "\n" +
-                                &deco + section + &deco + "\n" +
-                                &deco + &";".repeat(section.len()) + &deco};
+            let section_string: String = {";".to_string() + &deco.clone() + &"€".repeat(section.len()) + &deco + "\n" +
+                                ";" + &deco + section + &deco + "\n" +
+                                ";" + &deco + &"€".repeat(section.len()) + &deco};
             let section_entry = Entry{content: section_string,
                                       date: NaiveDate::from_ymd(1990, 1, 1),
                                       entry_type: EntryType::Section};
